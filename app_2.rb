99def68c4eb3e55914e324943e1a3fe2971ae2f8
Pol-Erwan <polerwankite@gmail.com>
2019-10-15 23:37:53 +0200

    update

--- app_2.rb
+++ app_2.rb
@@ -5,8 +5,8 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-def intro
-print " - "
+
+print " - "				# illustration d'intro
 sleep (0.1)
 print " - "
 sleep (0.1)
@@ -97,23 +97,22 @@ sleep (0.1)
 puts " - "
 sleep (0.1)
 
-end
 
-puts "Quel est ton prénom ?"
+puts "Quel est ton prénom ?"				# entrer son prénom
 print ">"
-player1 = HumanPlayer.new(gets.chomp)
+player1 = HumanPlayer.new(gets.chomp)			# rentrer le prénom dans le programme
 
-player2 = Player.new("Sangoku")
-player3 = Player.new("Végéta")
+player2 = Player.new("Sangoku")				# déclaration ennemi
+player3 = Player.new("Végéta")				# déclaration second ennei
 
-enemies = []
+enemies = []						# création tableau pour ranger les ennemis
 
-enemies << player2
+enemies << player2					# on met les ennemis dans le tableau
 enemies << player3
 
-while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points >0)
+while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points >0) #tant qu'un ennemi ou moi est en vie le combat continue
 
-  puts "Quelle action veux-tu effectuer ?"
+  puts "Quelle action veux-tu effectuer ?"		# menu choix des actions du jeu
 
   puts "a - chercher une meilleure arme"
   puts "s - chercher à se soigner" 
@@ -127,23 +126,22 @@ while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points
   print ">"
   choice = gets.chomp
 
-  if choice == "a"
+  if choice == "a"					# aller à la foncton corespondante du choix entré
     puts " /\/\/\ arme /\/\/\ "	
     puts player1.search_weapon
-    puts " "
   end
-  if choice == "s"
+  if choice == "s"					# aller à la foncton corespondante du choix entré
     puts " + + + Health Pack + + + "
     puts player1.search_health_pack
     puts " "
   end
-  if choice == "0"
+  if choice == "0"					# aller à la foncton corespondante du choix entré
     puts " "
     puts "o=)====>     FIGHT    <====(=o"
     player1.attacks(player2)
     puts " "
   end
-  if choice == "1"
+  if choice == "1"					# aller à la foncton corespondante du choix entré
     puts " "
     puts "o=)====>     FIGHT    <====(=o"
     player1.attacks(player3)
@@ -153,63 +151,32 @@ while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points
   puts "Les autres joueurs t'attaquent !"
   puts " "
   sleep (2)
-  player2.attacks(player1)
+    if player2.life_points > 0  
+      player2.attacks(player1)
+    end
   puts " "
   sleep (2)
-  player3.attacks(player1)
+    if player3.life_points > 0
+      player3.attacks(player1)
+    end
   puts " "
 
   #puts "tableau attack"
   #puts enemies
-  bot = 1
-  while bot < enemies.size
-    enemies.each do |bot|#attacks(player1)
-      puts bot
-      bot = bot + 1
-      bot.attacks(player1)
-    end
-  end
-  player2.attacks(player1)
-  enemies.attacks(player1)
+  #bot = 1
+  #while bot =< enemies.size
+    #enemies.each do |bot|
+      #puts bot
+      #bot.attacks(player1)
+    #end
+  #end
+  #player2.attacks(player1)
+  #enemies.attacks(player1)
 
   
-#bot.attacks(player1)
+
 sleep (1)
 end
-
-
-while player1.life_points > 0 && (player2.life_points > 0 || player3.life_points >0)
-
-  if player1.life_points > 0 && player2.life_points > 0
-    player1.attacks(player2)
-    puts player1.show_state
-  end
-
-  if player1.life_points > 0 && player3.life_points > 0
-    player1.attacks(player3)
-    puts player1.show_state
-  end
-
-  if player2.life_points > 0 && player1.life_points > 0
-    player2.attacks(player1)
-    puts player1.show_state
-  end
-  if player2.life_points > 0 && player3.life_points > 0
-    player2.attacks(player3)
-    puts player1.show_state
-  end
-	
-  if player3.life_points > 0 && player1.life_points > 0
-    player3.attacks(player1)
-    puts player1.show_state
-  end
-  if player3.life_points > 0 && player2.life_points > 0
-    player3.attacks(player2)
-    puts player1.show_state		
-  end		
-end
-
-
 
 puts "game over"
 
--- lib/player.rb
+++ lib/player.rb
@@ -1,43 +1,43 @@
 class Player
   attr_accessor :name, :life_points, :damage
 
-  def initialize(name)
+  def initialize(name)      		# j'initialise mes variables pour mon programme
     @name = name
     @life_points = 10
     @damage = damage
     damage = 0
   end
 
-  def show_state
+  def show_state			# ici je montre le statut du player
     puts "#{self.name} a #{self.life_points} points de vie."  
   end
 
-  def gets_damage(damage)
+  def gets_damage(damage)   		# ici je calcul les dommages
     @life_points = @life_points - damage
       if @life_points >= 0
-          puts "#{self.name} a #{@life_points} points de vie."
+          puts "#{self.name} a #{@life_points} points de vie." #affichage point restant
       else
-          puts "#{self.name} est mort"
+          puts "#{self.name} est mort"  		#affichage mort si plus de point
       end
   end
 
-  def attacks(player)
+  def attacks(player)			# ici je programme l'attaque
     puts "#{self.name} attaque #{player.name}"
-    damage = compute_damage
+    damage = compute_damage		# les dommages viennent du calcul avec le dé
     puts "-- > il lui inflige #{damage} points de dommage"
     player.gets_damage(damage) 
   end
 
-  def compute_damage
+  def compute_damage			# ici j'ai créé un dé qui calcul les dommages
     return rand(1..6)
   end
 
 end
 
-class HumanPlayer < Player
+class HumanPlayer < Player		# class avec un joueur qui plus d'options
 attr_accessor :weapon_level
 
-  def initialize(name)
+  def initialize(name)			# initialisation variable sur le joueur
     @name = name
     @life_points = 100
     @weapon_level = 1
@@ -45,18 +45,18 @@ attr_accessor :weapon_level
     damage = 0
   end
 
-  def show_state
+  def show_state			# ici je montre le statut du joueur
     puts "-> #{self.name} a #{self.life_points} points de vie et une arme de niveau #{self.weapon_level}"  
   end
 
-  def compute_damage
+  def compute_damage			# ici je calcul la force d'attaque avec mon niveau d'arme
     rand(1..6) * @weapon_level
   end
 
-  def search_weapon
+  def search_weapon			# ici je recherche une meilleur arme
     weapon_find = rand(1..6)
     puts "tu as trouvé une arme de niveau #{weapon_find}"
-    if weapon_find > weapon_level
+    if weapon_find > weapon_level	# comparaison entre arme actuelle et nouvelle arme
       puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
       	@weapon_level = weapon_find
 	puts "niveau de ton arme #{weapon_level}"
@@ -66,18 +66,18 @@ attr_accessor :weapon_level
     end
   end
 
-  def search_health_pack
+  def search_health_pack		# recherche point de vie	
     health_find = rand(1..6)
     if health_find == 1
-	puts "Tu n'as rien trouvé ... "
+	puts "Tu n'as rien trouvé ... " # point de vie trouvé = 1
     else
-      if health_find < 6	
+      if health_find < 6		# point de vie trouvé compris entre 2 et 5
 	puts "Bravo, tu as trouvé un pack de +50 points de vie !"
 	@life_points = @life_points + 50
 	  if @life_points > 100
             @life_points = 100
 	  end	
-      else 
+      else 				# point de vie trouvé = 6
 	puts "Waow, tu as trouvé un pack de +80 points de vie !"
 	@life_points = @life_points + 80
 	  if @life_points > 100
